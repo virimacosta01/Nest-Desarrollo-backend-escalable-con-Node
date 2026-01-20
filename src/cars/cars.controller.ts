298ef490ab03a86a057ff8967e2c10b0214748d3
virimacosta01 <acostaviridiana86@gmail.com>
2026-01-20 11:53:45 -0600

    Seccion 4: Interfaces y UUID

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -16,10 +16,10 @@ export class CarsController {
   }
 
   @Get(':id')  //
-  getCarById( @Param('id', ParseIntPipe) id: number) {
+  getCarById( @Param('id') id: string) {
 
     console.log({id})
-    return this.CarsService.findOneById(+id)
+    return this.CarsService.findOneById(id)
   }
 
   @Post()
